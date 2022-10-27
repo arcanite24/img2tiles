0b0483c905769b73692c0e288e67a3e9ded55623
Brandon G. Neri <arcaniteamp@gmail.com>
2022-10-27 01:17:40 -0500

    chore(): init

---
+++ README.md
@@ -0,0 +1,35 @@
+  # img2tiles
+  ### Simple AUTOMATIC1111 UI script to generate tiles from a base image
+  #### ⚠ WIP PRs welcome 😄
+
+  ## Installation
+  Place img2tiles.py into /YOUR_SD_FOLDER/scripts
+
+  ## Usage
+  - Go to img2img and load your base image (for now, only tested on 1:1 512x512 images)
+  - Choose **img2tiles** from the scripts select
+  - Adjust tile size (128 makes a 4x4 grid)
+  - Adjust overlap
+  - Choose your model and inference settings
+
+  ## Tips
+  - Pictures with highly recognizable patterns gives the best results
+  - Low steps count with high denoising strength (>0.8) on a sampling like Euler_a, Euler and DDIM outputs great results
+  - You can use any model you want, even Dreambooth ones
+  - You can erase tile divisions with inpainting (fun fact: Runway inpainting model doesn't work that great)
+
+  ## Examples
+  #### Mona Lisa 4x4
+  ![Mona Lisa made out of future factories](examples/example1.png "Mona Lisa 4x4")
+
+  ### Mona Lisa 8x8
+  ![Mona Lisa made out of future factories 8x8](examples/example2.png "Mona Lisa 8x8")
+
+  ### Selfie with a Dreambooth model of the same person as the original picture
+  ![Dreambooth selfie](examples/example3.png "Dreambooth selfie")
+
+  ### Van Gogh
+  ![Starry Night](examples/example4.png "Starry Night")
+
+  ## Credits
+  Based on **sd_upscale.py**
---
+++ img2tiles.py
@@ -0,0 +1,86 @@
+import math
+
+import modules.scripts as scripts
+import gradio as gr
+from PIL import Image
+
+from modules import processing, images, devices
+from modules.processing import Processed
+from modules.shared import state
+
+
+class Script(scripts.Script):
+    def title(self):
+        return "img2tiles"
+
+    def show(self, is_img2img):
+        return is_img2img
+
+    def ui(self, is_img2img):
+        tile_size = gr.Slider(minimum=32, maximum=256, step=16,
+                              label='Tile size', value=256, visible=True)
+
+        overlap = gr.Slider(minimum=0, maximum=256, step=16,
+                            label='Tile overlap', value=0, visible=True)
+
+        return [tile_size, overlap]
+
+    def run(self, p, tile_size, overlap):
+        processing.fix_seed(p)
+
+        initial_info = None
+        seed = p.seed
+
+        init_img = p.init_images[0]
+        img = init_img
+
+        devices.torch_gc()
+
+        batch_size = p.batch_size
+
+        grid = images.split_grid(
+            img, tile_w=tile_size, tile_h=tile_size, overlap=overlap)
+
+        work = []
+
+        for y, h, row in grid.tiles:
+            for tiledata in row:
+                work.append(tiledata[2])
+                # tiledata[2].show()
+
+        batch_count = math.ceil(len(work) / batch_size)
+        state.job_count = batch_count
+
+        print(
+            f"img2tiles will process a total of {len(work)} images tiled as {len(grid.tiles[0][2])}x{len(grid.tiles)} in a total of {state.job_count} batches.")
+
+        result_images = []
+        work_results = []
+
+        for i in range(batch_count):
+            p.batch_size = batch_size
+            p.init_images = work[i*batch_size:(i+1)*batch_size]
+
+            state.job = f"Batch {i + 1 * batch_count} out of {state.job_count}"
+            processed = processing.process_images(p)
+
+            if initial_info is None:
+                initial_info = processed.info
+
+            work_results += processed.images if batch_size == 1 else processed.images[1:]
+
+        shape = (len(grid.tiles[0][2]), len(grid.tiles))
+        image_size = (p.width * shape[1], p.height * shape[0])
+        combined_image = Image.new('RGB', image_size)
+
+        for row in range(shape[0]):
+            for col in range(shape[1]):
+                offset = p.width * col, p.height * row
+                idx = row * shape[1] + col
+                combined_image.paste(work_results[idx], offset)
+
+        result_images.append(combined_image)
+
+        processed = Processed(p, result_images, seed, initial_info)
+
+        return processed
